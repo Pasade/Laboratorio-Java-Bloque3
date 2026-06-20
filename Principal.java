e3ed31bd4dc03a41d61ee622d9d299b92cce21df
Jose Antonio Fernández Gago <joseantoniofernandezgago@gmail.com>
2026-06-20 15:58:12 +0000

    Modificado el main, creando un objeto a través de la clase PAquete, dandole atributos y usando la funcion mostrarDetalles

--- Principal.java
+++ Principal.java
@@ -1,21 +1,17 @@
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Sincronizado con GitHub Cloud");
-        System.out.println("PROBANDO: Nueva función de envíos internacionales v2");
 
-        // Un array con los pesos en kg de 5 paquetes en el almacén
-        double[] inventarioPesos = {10.5, 5.2, 22.4, 3.0, 15.8};
-        
-        System.out.println("--- CONTROL DE CARGA DEL ALMACÉN ---");
+        //Creamos "Objeto" p1 usando el molde(Clase)
+        Paquete p1 = new Paquete();
 
-        for(int i = 0; i < inventarioPesos.length; i ++){
-            inventarioPesos[i] *= 1.10;
-        }
+        //Le asignamos los atributos
+        p1.codigoSeguimiento = "TX-75201";
+        p1.peso = 8.4;
+        p1.destino = "Dallas";
 
-        System.out.println("Nuevo peso del primer paquete (con +10%): " + inventarioPesos[0] + "Kg");
-        System.out.println("Nuevo peso del tercer paquete (con +10%): " + inventarioPesos[2] + "Kg");
-        
-        
+        //Ejecutamos la funcion paramostrar los detalles
+        p1.mostrarDetalles();
     }
+
 }

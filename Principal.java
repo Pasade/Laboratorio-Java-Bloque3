8c387423c9c310928c1e581e74437f68556c5b8a
Jose Antonio Fernández Gago <joseantoniofernandezgago@gmail.com>
2026-07-02 04:07:39 +0000

    Provadas nuevas funciones de ArrayList

--- Principal.java
+++ Principal.java
@@ -13,6 +13,7 @@ public class Principal {
         //Usamos el cosntructor para crear dos paquetes
         Paquete p1 = new Paquete(8.4, "Dallas");
         Paquete p2 = new Paquete(14.2);
+        Paquete p3 = new Paquete(23.4, "Madrid");
         //Modificamos los paquetes a petición del cliente
         p1.setDestino("Ribeira");
         p1.setPeso(10);
@@ -29,9 +30,34 @@ public class Principal {
             p.mostrarDetalles();
         }
 
-        //Indicamos cuantos elementos hay dentro del Array
-        System.out.println("\nTotal de paquetes en el sistema elástico: " + listaDeEnvios.size());
+        //Creamos un condicional para que compruebe si el camión está vacio
+        if(listaDeEnvios.isEmpty()){
+            System.out.println("No hay paquetes para enviar.");
+        }
+        else{
+            //Indicamos cuantos elementos hay dentro del Array
+            System.out.println("\nTotal de paquetes en el sistema elástico: " + listaDeEnvios.size() + "/10");
+        }
 
+        listaDeEnvios.remove(1);
+        listaDeEnvios.add(p3);
+
+        
+        //Mostramos todos los paquetes que hay en el Array
+        System.out.println("\n🚚 LISTADO DE PAQUETES REGISTRADOS EN EL ARRAYLIST:");
+        for(Paquete p : listaDeEnvios){
+            p.mostrarDetalles();
+        }
+
+        //Creamos un condicional para que compruebe si el camión está vacio
+        if(listaDeEnvios.isEmpty()){
+            System.out.println("No hay paquetes para enviar.");
+        }
+        else{
+            //Indicamos cuantos elementos hay dentro del Array
+            System.out.println("\nTotal de paquetes en el sistema elástico: " + listaDeEnvios.size() + "/10");
+        }        
+        
 
     }
 

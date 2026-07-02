23178a6de1f657f4fcdaf3e3f61f2657ead34a34
Jose Antonio Fernández Gago <joseantoniofernandezgago@gmail.com>
2026-07-02 03:34:07 +0000

    Agregado array dinámico para crear una lista de paquetes

--- Principal.java
+++ Principal.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Principal {
     
     public static void main(String[] args) {
@@ -5,20 +7,32 @@ public class Principal {
         //Prueba de funciones en un archivo a parte
         Funciones.bienvenidaGestorEnvios();
 
-        //Usamos la clase creada con el constructor
-        Paquete p1 = new Paquete(8.4, "Dallas");
+        //Creamos un Array dinámico para poder meter los paquetes (Como si fuera un camión)
+        ArrayList<Paquete> listaDeEnvios = new ArrayList<>();
 
+        //Usamos el cosntructor para crear dos paquetes
+        Paquete p1 = new Paquete(8.4, "Dallas");
+        Paquete p2 = new Paquete(14.2);
+        //Modificamos los paquetes a petición del cliente
         p1.setDestino("Ribeira");
         p1.setPeso(10);
 
         //Ejecutamos la función para mostrar detalles
-        p1.mostrarDetalles();
+        // p1.mostrarDetalles();
 
-        //Creamos un segundo paquete usando el constructor para los casos en los que falta el dato destino
-        Paquete p2 = new Paquete(14.2);
+        //Añadiremos los paquetes al Array dinámico
+        listaDeEnvios.addAll(List.of(p1,p2));
 
-        //Ejecutamos la funcion paramostrar los detalles
-        p2.mostrarDetalles();
+        //Mostramos todos los paquetes que hay en el Array
+        System.out.println("\n🚚 LISTADO DE PAQUETES REGISTRADOS EN EL ARRAYLIST:");
+        for(Paquete p : listaDeEnvios){
+            p.mostrarDetalles();
+        }
+
+        //Indicamos cuantos elementos hay dentro del Array
+        System.out.println("\nTotal de paquetes en el sistema elástico: " + listaDeEnvios.size());
+
+
     }
 
 }
